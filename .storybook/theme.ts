348b979b7044bda684ddef010189ff4ce75e55b9
JRhodes95 <23459058+JRhodes95@users.noreply.github.com>
2023-09-29 15:15:23 +0100

    2 files added, 1 deleted

---
+++ .storybook/theme.ts
@@ -0,0 +1,8 @@
+import { create } from '@storybook/theming/create';
+
+export default create({
+  base: 'light',
+  brandTitle: 'Our Design System',
+  brandUrl: 'https://example.com',
+  brandImage: '../src/assets/sample.png',
+});
